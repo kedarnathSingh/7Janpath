aba75023f29aa61400a64199b08a8142a9b05320
Kedar nath Singh <nathkd@gmail.com>
2024-10-20 15:56:33 +0530

    currency data

--- clientapp/app/components/body/currency-list.tsx
+++ clientapp/app/components/body/currency-list.tsx
@@ -13,12 +13,12 @@ export default function Currencylist(){
     useEffect( () => {
           async function fetchData() {
               try {
-                  let response = await fetch('https://dummyjson.com/products');
+                  let response = await fetch(`${process.env.basePath}/currencies/list`);
                   if (!response.ok) {
                       throw new Error('Network response was not ok');
                   }
                   let data = await response.json();
-                  setCurrency(data.products);
+                  setCurrency(data);
               } catch (error) {
                   console.error('Error fetching data:', error);
               }
@@ -27,26 +27,25 @@ export default function Currencylist(){
 
     }, [])
     return(
-        <div>
-            <h1>Currency List</h1>
-            
+        <div className="head-container-sec mb-5 w-100">
+            <h2>Today's Buy & Sell rate</h2>  
         <table className="table custom-table-set">
 						<thead>
 						  <tr>
 							<th scope="col">Currency</th>
 							<th scope="col">Buy</th>
 							<th scope="col">Sell</th>
-							<th scope="col">Our Rate</th>
+							<th scope="col">Rate</th>
 						  </tr>
 						</thead>
 						<tbody>
                         {
-    currency.map((item: { id: number; title: string }) => (
+    currency.map((item: { id: number; name: string, symbol: string, rate: number, priority: number, buy_rate: number, sell_rate: number }) => (
 						  <tr>
-							<td>{item.title}</td>
-							<td>{item.id}</td>
-							<td>{item.id}</td>
-							<td>{item.id}</td>
+							<td>{item.name} <small className="small">{item.symbol}</small></td>
+							<td>{item.buy_rate}</td>
+							<td>{item.sell_rate}</td>
+							<td>{item.rate}</td>
 						  </tr>
                              ))
 }
