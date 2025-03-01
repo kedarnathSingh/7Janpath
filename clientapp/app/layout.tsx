6f61fb208f9c850b8e5a741fb76c48847d66822e
Ashutosh Kumar <ashutoshkumar@Ashutoshs-MacBook-Air.local>
2025-03-01 17:10:43 +0530

    google monetering script added

--- clientapp/app/layout.tsx
+++ clientapp/app/layout.tsx
@@ -1,8 +1,43 @@
+// import type { Metadata } from 'next'
+// import { Inter } from 'next/font/google'
+// import './globals.scss'
+// import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+// import  BootstrapClient from './components/BootstrapClient';
+
+// const inter = Inter({ subsets: ['latin'] })
+
+// export const metadata: Metadata = {
+//   title: {
+//     default: '7TravelMoney',
+//     template: '%s'
+//   }
+// }
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+      
+//       <body className={inter.className}>
+//         {children}
+//         <BootstrapClient />
+//         </body>
+//         {/* <!-- Google tag (gtag.js) --> */}
+//     </html>
+    
+//   )
+// }
+
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.scss'
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
-import  BootstrapClient from './components/BootstrapClient';
+import BootstrapClient from './components/BootstrapClient';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,10 +55,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DXPZ2QBPXS" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DXPZ2QBPXS');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
         <BootstrapClient />
-        </body>
+      </body>
     </html>
   )
 }
