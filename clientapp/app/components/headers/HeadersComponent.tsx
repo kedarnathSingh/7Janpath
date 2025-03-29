ddaaceb9b20c7d4d50fe490a61bbeef8a930a43c
Ashutosh Kumar <ashutoshkumar@Ashutoshs-MacBook-Air.local>
2025-03-29 15:09:37 +0530

    css media query fixes

--- clientapp/app/components/headers/HeadersComponent.tsx
+++ clientapp/app/components/headers/HeadersComponent.tsx
@@ -22,7 +22,7 @@ const HeadersComponent = () => {
 				<nav className="navbar navbar-expand-lg custom-nav ">
 					<div className="container-fluid">
 						<Link className="navbar-brand" href={'/home'}>
-							<img className="pt-0" src="images/logo.svg" />
+							<img className="logo" src="images/logo.svg" />
 						</Link>
 						<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span className="navbar-toggler-icon"></span>
